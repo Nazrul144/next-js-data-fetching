7db3f18f2ff06b00e4d3f9f9b7fbfe0e18952c2a
Nazrul144 <nazrulislam.cse28@gmail.com>
2025-02-01 20:49:16 +0600

    Loader added

---
+++ src/app/posts/loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loading = () => {
+    return (
+        <div>
+            <h1>Post data is loading........</h1>
+        </div>
+    );
+}
+
+export default Loading;
