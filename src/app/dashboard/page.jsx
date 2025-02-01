1253d12c3b40e314010d4e4df4c1d6673556c5e5
Nazrul144 <nazrulislam.cse28@gmail.com>
2025-02-01 16:05:20 +0600

    Navagating Programatically by push

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,9 +1,20 @@
+"use client"
+
+import { useRouter } from 'next/navigation';
 import React from 'react';
 
 const DashboardPage = () => {
+    const router = useRouter()
+    const orderConfirm = ()=>{
+        alert("Order has been confirmed.")
+        router.push('/about')
+    }
+
+
     return (
         <div>
             <h1>This is dashboard</h1>
+            <button onClick={orderConfirm} className='bg-sky-400 p-2 rounded-md'>Confirm Order</button>
         </div>
     );
 }
