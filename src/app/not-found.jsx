7066c3b749d568a0fa79d07f8e15538451db1c87
Nazrul144 <nazrulislam.cse28@gmail.com>
2025-02-01 16:16:16 +0600

    404 page implemented

---
+++ src/app/not-found.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div className='flex justify-center items-center h-screen'>
+            <h1 className='font-bold text-4xl text-red-500'>404 | Ops! Page Not Found.</h1>
+        </div>
+    );
+}
+
+export default NotFound;
